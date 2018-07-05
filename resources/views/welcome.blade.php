ebc04eb1587e34fa8ad8d932d61c76e30082cc9d
jiro.kirameki <ec2-user@ip-172-31-43-201.us-east-2.compute.internal>
2018-07-05 07:57:29 +0000

    login and registration at welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,6 +35,17 @@
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+                <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
